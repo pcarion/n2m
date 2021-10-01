2fcd89623ecc55d8dec23fc92379d445d8087880
Pierre Carion <pcarion@gmail.com>
2021-09-30 19:34:25 -0700

    test: get page content

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 
@@ -12,17 +13,36 @@ func main() {
 	notionIntegrationToken := os.Getenv("NOTION_INTEGRATION_TOKEN")
 
 	if notionIntegrationToken == "" {
-		fmt.Println("Missing environment variable: NOTION_INTEGRATION_TOKEN")
+		fmt.Printf("Missing environment variable: NOTION_INTEGRATION_TOKEN\n")
 		os.Exit(1)
 	}
 
-	fmt.Println("Hello from test")
 	client := notionapi.NewClient(notionapi.Token(notionIntegrationToken))
 
 	page, err := client.Page.Get(context.Background(), "8ebca155ffda45f7b5d49b0892672dea")
 	if err != nil {
-		fmt.Printf("error: %v", err)
+		fmt.Printf("error: %v\n", err)
 		os.Exit(1)
 	}
-	fmt.Printf("page: %v", page)
+	fmt.Printf("page: %v\n", page)
+
+	pageData, err := json.Marshal(page)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("pageData: %s\n", string(pageData))
+
+	block, err := client.Block.GetChildren(context.Background(), notionapi.BlockID(page.ID), nil)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+	blockData, err := json.Marshal(block)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("blockData: %s\n", string(blockData))
+
 }
